c55c38903365474a80cfc70ed99bd545586caa64
emon <khorshedemon066@gmail.com>
2021-03-30 21:17:16 +0600

    v1

---
+++ config.js
@@ -0,0 +1,24 @@
+var config = {
+    texts: [
+        "Send to Khorshed Alom",      
+        "Beloved Soto Vai", 
+        "Today is your birthday",
+        "Wish you happy returns of the day",
+        "শুভ পয়দা দিবস",
+        "অনেক বড় হও",
+        "Lots Of love",
+    ],
+    imgs: {
+        "শুভ পয়দা দিবস": "./imgs/xiaokeai.jpg",
+    },
+    desc: {
+        turn_on: "Start",
+        play: "music",
+        bannar_coming: "Bannar",
+        balloons_flying: "Seems to have something less",
+        cake_fadein: "cake?",
+        light_candle: "candle?",
+        wish_message: "happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
