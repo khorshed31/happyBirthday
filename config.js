83a68f6b3cab76d37172bc0df0503a1e19d44209
Khorshed <khorshedalom3517@gmail.com>
2021-06-17 00:47:29 +0600

    v4

--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 var config = {
     texts: [
         "Send to Khorshed Alom",      
-        "Beloved Soto Vai", 
+        "Beloved Soto dustu bon", 
         "Today is your birthday",
         "Wish you happy returns of the day",
         "শুভ পয়দা দিবস",
         "অনেক বড় হও",
-        " Masti bole to Kurkure",
         "Lots Of love",
     ],
     imgs: {
